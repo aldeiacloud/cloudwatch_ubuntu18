597a26d9ef4a3fb2c7928cd1dccde5558b3ec504
AldeiaCloud <78882441+aldeiacloud@users.noreply.github.com>
2021-04-17 10:13:39 -0300

    Create install_sp.sh

---
+++ install_sp.sh
@@ -0,0 +1,14 @@
+# WGET COM AGENT DE SAO PAULO
+wget https://s3.us-east-1.amazonaws.com/amazoncloudwatch-agent-sa-east-1/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb 
+# INSTALACAO DO AGENT
+dpkg -i -E ./amazon-cloudwatch-agent.deb 
+cd /opt/aws/amazon-cloudwatch-agent/bin 
+# BAIXAR SCRIPT DE COLETA (BASICO) PARA LINUX
+wget https://raw.githubusercontent.com/aldeiacloud/cloudwatch_ubuntu18/main/config.json 
+cd /opt/aws/amazon-cloudwatch-agent/etc/ 
+cp -av ../bin/config.json amazon-cloudwatch-agent.json 
+mkdir -p /usr/share/collectd/ 
+touch /usr/share/collectd/types.db 
+sudo sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s 
+systemctl enable amazon-cloudwatch-agent 
+systemctl status amazon-cloudwatch-agent
